e10ef33f4116fa51290f1d3bad9b43d32407f992
gbrozdev <gbrozdev@gmail.com>
2021-11-28 21:16:49 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,28 @@ router.get('/', async function (req, res) {
   res.render('index');
 });
 
+router.get('/add', async function (req, res) {
+  res.render('add');
+});
+
+router.get('/bca', async function (req, res) {
+  res.render('semester');
+});
+
+router.get('/semester1', async function (req, res) {
+  res.render('subject');
+});
+
+router.get('/english', async function (req, res) {
+  res.render('type');
+});
+
+router.get('/notes', async function (req, res) {
+  res.render('files');
+});
+
+router.get('/form', async function (req, res) {
+  res.render('form');
+});
+
 module.exports = router;
