562aeca34148a300fe49c2fe167a7706499fcbaf
gbrozdev <gbrozdev@gmail.com>
2021-11-28 22:03:12 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,17 @@ var db = require('../connection')
 
 /* GET home page. */
 router.get('/', async function (req, res) {
-  res.render('index');
+  let items = await db.get().collection('items').find().toArray()
+  res.render('index',{items});
 });
 
 router.get('/add', async function (req, res) {
   res.render('add');
 });
 
-router.get('/bca', async function (req, res) {
-  res.render('semester');
+router.get('/:id', async function (req, res) {
+  let itemid = req.params.id
+  res.render('semester',{itemid});
 });
 
 router.get('/semester1', async function (req, res) {
@@ -30,6 +32,16 @@ router.get('/notes', async function (req, res) {
 
 router.get('/form', async function (req, res) {
   res.render('form');
+});
+
+router.post('/add', async function (req, res) {
+  let item = req.body
+  db.get().collection('items').insertOne(item)
+  res.redirect('/');
+});
+
+router.get('/:itemid/add', async function (req, res) {
+  res.render('add');
 });
 
 module.exports = router;
