d3ee4983b920713c1763c9fcca2d9399dfe027e1
gbrozdev <gbrozdev@gmail.com>
2021-11-28 19:56:33 +0530

    new

--- src/index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW registerd");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW registration failed");
-        console.log(error);
-    })
-}
