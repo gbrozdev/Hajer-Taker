0c2c1db8ce4b5e0fb8486b873c3522049ad83259
gbrozdev <gbrozdev@gmail.com>
2021-11-29 00:32:57 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,70 @@ var db = require('../connection')
 
 /* GET home page. */
 router.get('/', async function (req, res) {
-  let items = await db.get().collection('items').find().toArray()
-  res.render('index',{items});
+  let data = await db.get().collection('data').find().toArray()
+  res.render('index',{data});
 });
 
 router.get('/add', async function (req, res) {
   res.render('add');
 });
 
-router.get('/:id', async function (req, res) {
-  let itemid = req.params.id
-  res.render('semester',{itemid});
+router.post('/add', async function (req, res) {
+  let data = req.body
+  db.get().collection('data').insertOne(data)
+  res.redirect('/');
 });
+
+router.get('/:course', async function (req, res) {
+  course = req.params.course
+  let data = await db.get().collection('data').find({"course":course}).toArray()
+  res.render('semester',{course});
+});
+
+router.get('/:course/add', async function (req, res) {
+  let course = req.params.course
+  res.render('add',{course});
+});
+
+
+
+// router.get('/:course/add', async function (req, res) {
+//   let semester = req.body
+//   db.get().collection('semester').insertOne(semester)
+//   res.redirect('/semester1');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.get('/semester1', async function (req, res) {
   res.render('subject');
 });
 
-router.get('/english', async function (req, res) {
-  res.render('type');
-});
+// router.get('/english', async function (req, res) {
+//   res.render('type');
+// });
 
-router.get('/notes', async function (req, res) {
-  res.render('files');
-});
+// router.get('/notes', async function (req, res) {
+//   res.render('files');
+// });
 
-router.get('/form', async function (req, res) {
-  res.render('form');
-});
+// router.get('/form', async function (req, res) {
+//   res.render('form');
+// });
 
-router.post('/add', async function (req, res) {
-  let item = req.body
-  db.get().collection('items').insertOne(item)
-  res.redirect('/');
-});
-
-router.get('/:itemid/add', async function (req, res) {
-  res.render('add');
-});
 
 module.exports = router;
