3a3b9e750d1e7180f0a321b64389ebb087268bf7
gbrozdev <gbrozdev@gmail.com>
2021-12-01 15:29:10 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,27 @@ var db = require('../connection')
 /* GET home page. */
 router.get('/', async function (req, res) {
   if (req.session.admin === true) {
-    res.render('index',{admin});
+    res.render('index', {admin:true});
+  } else {
+    res.render('index');
   }
-  res.render('index');
-  
+});
+
+router.get('/delete/:id', async function (req, res) {
+  let id = req.params.id
+  console.log(id);
+  db.get().collection('data').deleteOne({ "name": id })
+  res.redirect('back')
 });
 
 router.get('/courses', async function (req, res) {
   req.session.url = req.route.path
-  console.log(req.session.url);
-  let data = await db.get().collection('data').find({"item":"courses"}).toArray()
-  res.render('courses',{data});
+  let data = await db.get().collection('data').find({ "item": "courses" }).toArray()
+  if (req.session.admin === true) {
+    res.render('courses',{data,admin:true});
+  } else {
+    res.render('courses',{data});
+  }
 });
 
 router.get('/add:parameter', async function (req, res) {
@@ -44,7 +54,7 @@ router.post('/admin', async function (req, res) {
   let admindata = req.body
   if (admindata.gmail === "gbroz@123",admindata.password === "9846551975") {
     req.session.admin = true
-  res.render('index');
+  res.redirect('/');
   } else {
     res.render('admin');
 }
@@ -57,20 +67,24 @@ router.get('/videos/:course/:semester/:subject', async function (req, res) {
   let videoid = ('videos'+course + semester + subject)
   url = 'videos' + '/' + course+'/'+semester+'/'+subject
   req.session.url = url
-  let uploads = await db.get().collection('uploads').find({"item":videoid}).toArray()
-  
-
-  
- uploads = await db.get().collection('uploads').find({"item":videoid}).toArray()
-  res.render('videos',{course,semester,subject,uploads});
+  let uploads = await db.get().collection('uploads').find({ "item": videoid }).toArray()
+  if (req.session.admin === true) {
+    res.render('videos',{course,semester,subject,uploads,admin:true});
+  } else {
+    res.render('videos',{course,semester,subject,uploads});
+  }
 });
 
 router.get('/course/:course', async function (req, res) {
   course = req.params.course
   url = '/course/' + course
   req.session.url = url
-  let data = await db.get().collection('data').find({ "item":course}).toArray()
-  res.render('semester', { data, course });
+  let data = await db.get().collection('data').find({ "item": course }).toArray()
+  if (req.session.admin === true) {
+    res.render('semester',{data, course, admin:true});
+  } else {
+    res.render('semester', { data, course });
+  }
 });
 
 router.get('/:course/:semester', async function (req, res) {
@@ -79,8 +93,12 @@ router.get('/:course/:semester', async function (req, res) {
   let subjectid = (course + semester);
   url = course+'/'+semester
   req.session.url = url
-  let data = await db.get().collection('data').find({ "item":subjectid}).toArray()
-  res.render('subject',{course,semester,data});
+  let data = await db.get().collection('data').find({ "item": subjectid }).toArray()
+  if (req.session.admin === true) {
+    res.render('subject',{course,semester,data,admin:true});
+  } else {
+    res.render('subject',{course,semester,data});
+  }
 });
 
 router.get('/:course/:semester/:subject', async function (req, res) {
@@ -90,8 +108,12 @@ router.get('/:course/:semester/:subject', async function (req, res) {
   let typeid = (course + semester + subject)
   url = course+'/'+semester+'/'+subject
   req.session.url = url
-  let data = await db.get().collection('data').find({ "item":typeid}).toArray()
-  res.render('type',{course,semester,subject,data});
+  let data = await db.get().collection('data').find({ "item": typeid }).toArray()
+  if (req.session.admin === true) {
+    res.render('type',{course,semester,subject,data,admin:true});
+  } else {
+    res.render('type',{course,semester,subject,data});
+  }
 });
 
 router.get('/:course/:semester/:subject/:type', async function (req, res) {
@@ -102,8 +124,12 @@ router.get('/:course/:semester/:subject/:type', async function (req, res) {
   let fileid = (course + semester + subject +type)
   url = course+'/'+semester+'/'+subject+'/'+type
   req.session.url = url
-  let uploads = await db.get().collection('uploads').find({"item":fileid}).toArray()
-  res.render('files',{course,semester,subject,type,uploads});
+  let uploads = await db.get().collection('uploads').find({ "item": fileid }).toArray()
+  if (req.session.admin === true) {
+    res.render('files',{course,semester,subject,type,uploads,admin:true});
+  } else {
+    res.render('files',{course,semester,subject,type,uploads});
+  }
 });
 
 router.get('/upload:parameter', async function (req, res) {
@@ -120,6 +146,7 @@ router.post('/upload', async function (req, res) {
   url = req.session.url
   res.redirect(url);
 });
+
 
 
 
